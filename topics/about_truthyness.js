33bc2e62c9935dc09dd8549866c51bf45f22228f
Nate Hoellein <natehoellein@gmail.com>
2013-05-03 15:37:55 -0400

    Added topics/about_truthyness

--- topics/about_truthyness.js
+++ topics/about_truthyness.js
@@ -1,4 +1,26 @@
+describe("About Truthyness",function() {
+  
+  it("for positive numbers", function() {
+    var oneIsTruthy = 1 ? true : false;
+    expect(oneIsTruthy).toBe(true);  // 'is one truthy?');
+  });
 
+  it("for negative numbers", function() {
+    var negativeOneIsTruthy = -1 ? true : false;
+    expect(negativeOneIsTruthy).toBe(true);  // 'is -1 truthy?');
+  });
+
+  it("for zero", function() {
+    var zeroIsTruthy = 0 ? true : false;
+    expect(zeroIsTruthy).toBe(false);  // 'is 0 truthy?');
+  });
+
+  it("for null", function() {
+    var nullOneIsTruthy = null ? true : false;
+    expect(nullOneIsTruthy).toBe(false);  // 'is null truthy?');
+  });
+});
+/*
 module("About Truthyness (topics/about_truthyness.js)");
 
 test("truthyness of positive numbers", function() {
@@ -20,3 +42,4 @@ test("truthyness of null", function() {
     var nullIsTruthy = null ? true : false;
     equals(nullIsTruthy, __, 'is null truthy?');
 });
+*/
