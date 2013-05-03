d388cc7c4d6e7275caa33d593ca219039d2c8b4b
Nate Hoellein <natehoellein@gmail.com>
2013-05-03 14:04:50 -0400

    1.  changed about_asserts to about_expects 2.  Updated about_operators

--- topics/about_operators.js
+++ topics/about_operators.js
@@ -1,4 +1,51 @@
+describe("About Operators (topics/about_operators.js", function() {
+  it("Addition", function() {
+    var result = 0;
+    //starting i at 0, add i to result and increment i by 1 until i is equal to 5
+    for (var i = 0; i <= 5; i++) {
+      result = result + i;
+    }
+    expect(result).toBe(15); // "What is the value of result?");
+  });
+  
+  it("assignment addition", function() {
+    var result = 0;
+    for (var i = 0; i <=5; i++) {
+      //the code below is just like saying result = result + i; but is more concise
+      result += i;
+    }
+    expect(result).toBe(15); // "What is the value of result?");
+  });
 
+  it("subtraction", function() {
+    var result = 5;
+    for (var i = 0; i <= 2; i++) {
+      result = result - i;
+    }
+    expect(result).toBe(2); // "What is the value of result?");
+  });
+
+  it('assignment subtraction', function() {
+    var result = 5;
+    for (var i = 0; i <= 2; i++) {
+      result -= i;
+    }
+    expect(result).toBe(2); // "What is the value of result?");
+  });
+
+//Assignment operators are available for multiplication and division as well
+//let's do one more, the modulo operator, used for showing division remainder
+
+  it("modulus", function() {
+    var result = 10;
+    var x = 5;
+    //again this is exactly the same as result = result % x
+    result %= x;
+    expect(result).toBe(0);  // "What is the value of result?");
+  });
+  
+});
+/*
 module("About Operators (topics/about_operators.js)");
 
 test("addition", function() {
@@ -45,3 +92,4 @@ test("modulus", function() {
   result %= x;
   equals(result, __, "What is the value of result?");
 });
+*/
