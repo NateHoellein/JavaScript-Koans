51c80f91b8378c36ac6f1beed393e5ca769064a2
Nate Hoellein <natehoellein@gmail.com>
2013-05-03 15:45:13 -0400

    Added about_assignment

--- topics/about_assignment.js
+++ topics/about_assignment.js
@@ -1,4 +1,16 @@
+describe("About Assignment",function() {
+  it("scopes local variables", function() {
+    var temp = 1;
+    expect(temp).toBe(1);
+  });
 
+  it("scopes global variables", function() {
+    temp = 5;
+    expect(temp).toBe(5);
+  });
+});
+
+/*
 module("About Assignment (topics/about_assignment.js)");
 
 test("local variables", function() {
@@ -10,3 +22,4 @@ test("global variables", function() {
     temp = 1;
     equals(temp, window.__, 'global variables are assigned to the window object');
 });
+*/
