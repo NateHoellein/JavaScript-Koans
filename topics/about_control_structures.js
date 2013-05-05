e5e99bfeefe4bc35dbb2c7c5b24fb0a8403cc6e5
Nate Hoellein <natehoellein@gmail.com>
2013-05-05 10:08:23 -0400

    Added topics/about_control_structures

--- topics/about_control_structures.js
+++ topics/about_control_structures.js
@@ -1,3 +1,63 @@
+describe("About Control Structures (topics/about_control_structures.js)", function() {
+
+  it("if", function() {
+    var isPositive = false;
+    if (2 > 0) {
+      isPositive = true;
+    }
+    expect(isPositive).toBe(true);  // 'what is the value of isPositive?');
+  });
+
+
+  it("for", function() {
+    var counter = 10;
+    for (var i = 1; i <= 3; i++) {
+      counter = counter + i;
+    }
+    expect(counter).toBe(16); // 'what is the value of counter?');
+  });
+
+  it("for in", function() {
+    // this syntax will be explained in about objects
+    var person = {
+      name: "Amory Blaine",
+      age: 102
+    };
+    var result = "";
+    // for in enumerates the property names of an object
+    for (property_name in person) {
+    result = result + property_name;
+    };
+    expect(result).toBe("nameage"); // 'what is the value of result?');
+  });
+
+  it("ternary operator", function() {
+    var fruit = true ? "apple" : "orange";
+    expect(fruit).toBe("apple"); // 'what is the value of fruit?');
+
+    fruit = false ? "apple" : "orange";
+    expect(fruit).toBe("orange"); // 'now what is the value of fruit?');
+  });
+
+  it("switch", function() {
+    var result = 0;
+    switch (2) {
+      case 1:
+        result = 1;
+        break;
+      case 1+1:
+        result = 2;
+        break;
+    }
+    expect(result).toBe(2); // 'what is the value of result?');
+  });
+
+  it("null coalescing", function() {
+    var result = null || "a value";
+    expect(result).toBe("a value"); // 'what is the value of result?');
+  });
+});
+/*
 module("About Control Structures (topics/about_control_structures.js)");
 
 test("if", function() {
@@ -71,3 +131,4 @@ test("null coalescing", function() {
     var result = null || "a value";
     equals(result, __, 'what is the value of result?');
 });
+*/
