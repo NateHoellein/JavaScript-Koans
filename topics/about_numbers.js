b7543b7d32b53296479f732455831eb995c50358
Nate Hoellein <natehoellein@gmail.com>
2013-05-06 10:17:53 -0400

    updated to add about_numbers and about_objects

--- topics/about_numbers.js
+++ topics/about_numbers.js
@@ -1,4 +1,20 @@
+describe("About Numbers (topics/about_numbers.js)", function() {
 
+  it("types", function() {
+      var typeOfIntegers = typeof(6);
+      var typeOfFloats = typeof(3.14159);
+      expect(typeOfIntegers).toBe(typeOfFloats); // 'are ints and floats the same type?');
+      expect(typeOfIntegers).toBe("number"); // 'what is the javascript numeric type?');
+      expect(1.0, 1); // 'what is a integer number equivalent to 1.0?');
+  });
+
+  it("NaN", function() {
+      var resultOfFailedOperations = 7/'apple';
+      expect(isNaN(resultOfFailedOperations)).toBe(true); // 'what will satisfy the equals assertion?');
+      expect(resultOfFailedOperations).not.toEqual(NaN); // 'is NaN == NaN?');
+  });
+});
+/*
 module("About Numbers (topics/about_numbers.js)");
 
 test("types", function() {
@@ -14,3 +30,4 @@ test("NaN", function() {
     equals(isNaN(resultOfFailedOperations), __, 'what will satisfy the equals assertion?');
     equals(resultOfFailedOperations == NaN, __, 'is NaN == NaN?');
 });
+*/
--- topics/about_objects.js
+++ topics/about_objects.js
@@ -1,4 +1,49 @@
+describe("About Objects (topics/about_objects.js)",function() {
 
+
+  it("object type", function() {
+      var empty_object = {};
+      expect(typeof(empty_object)).toBe("object"); // 'what is the type of an object?');
+  });
+
+  it("object literal notation", function() {
+      var person = {
+          name:"Amory Blaine",
+          age:102
+      };		
+      expect(person.name).toBe("Amory Blaine"); // 'what is the person\'s name?');
+      expect(person.age).toBe(102); // 'what is the person\'s age?');
+  });
+
+  it("dynamically adding properties", function() {
+      var person = {};
+      person.name = "Amory Blaine";
+      person.age = 102;
+      expect(person.name).toBe("Amory Blaine"); // 'what is the person\'s name?');
+      expect(person.age).toBe(102); // 'what is the person\'s age?');
+  }); 
+
+  it("adding properties from strings", function() {
+      var person = {};
+      person["name"] = "Amory Blaine";
+      person["age"] = 102;
+      expect(person.name).toBe("Amory Blaine"); // 'what is the person\'s name?');
+      expect(person.age).toBe(102); // 'what is the person\'s age?');
+  });
+
+  it("adding functions", function() {
+      var person = {
+          name: "Amory Blaine",
+          age: 102,
+          toString: function() {
+              return "I " + this.name + " am " + this.age + " years old.";  // HINT: use the 'this' keyword to refer to the person object.
+          }
+      };
+      expect(person.toString()).toBe("I Amory Blaine am 102 years old."); // 'what should the toString function be?');
+  });
+
+});
+/*
 module("About Objects (topics/about_objects.js)");
 
 test("object type", function() {
@@ -41,3 +86,4 @@ test("adding functions", function() {
     };
     equals(person.toString(), "I Amory Blaine am 102 years old.", 'what should the toString function be?');
 });
+*/
