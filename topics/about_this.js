5c8ba3ca5d6f1f2829eb2e4b4aeb3db6acb26e88
Nate Hoellein <natehoellein@gmail.com>
2013-05-06 14:38:32 -0400

    updated for topics/about_this.js

--- topics/about_this.js
+++ topics/about_this.js
@@ -1,3 +1,48 @@
+
+describe("About this (topics/about_this.js)",function() {
+
+  it("'this' inside a method", function () {
+    var person = {
+      name: 'bob',
+      intro: function () {
+        return "Hello, my name is " + this.name;
+      } 
+    }
+    expect(person.intro()).toBe("Hello, my name is bob");
+  });
+
+
+  it("'this' on unattached function", function () {
+    var person = {
+      name: 'bob',
+      intro: function () {
+        return "Hello, my name is " + this.name;
+      } 
+    }
+
+    var alias = person.intro;
+    
+    // if the function is not called as an object property 'this' is the global context 
+    // (window in a browser)
+    window.name = 'Peter';
+    expect("Hello, my name is Peter").toBe(alias());
+  });
+
+  it("'this' set explicitly", function () {
+    var person = {
+      name: 'bob',
+      intro: function () {
+        return "Hello, my name is " + this.name;
+      } 
+    }
+
+    // calling a function with 'call' lets us assign 'this' explicitly
+    var message = person.intro.call({name: "Frank"});
+    expect(message).toBe("Hello, my name is Frank");
+  });
+
+});
+/*
 module("About this (topics/about_this.js)");
 
 test("'this' inside a method", function () {
@@ -41,4 +86,4 @@ test("'this' set explicitly", function () {
 
 // extra credit: underscore.js has a 'bind' function http://documentcloud.github.com/underscore/#bind
 // read the source and see how it is implemented
-
+*/
