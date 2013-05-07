ec217bb95fe58e17f980ec971bcb5f2197bdf6dd
Nate Hoellein <natehoellein@gmail.com>
2013-05-07 11:15:22 -0400

    changed about_expects to jasmine's truthyness/falsyness

--- topics/about_expects.js
+++ topics/about_expects.js
@@ -1,28 +1,14 @@
-
 describe("About Expects (topics/about_expects.js)", function(){
   
-  it("comparasion using 'toBe()'",function() {
-      expect(true).toBe(true);
+  it("looking for truthyness",function() {
+      expect(false).toBeTruthy();
   });
 
-  it("demonstrates how to negate 'toBe()'", function() {
-    expect(false).not.toBe(true);
+  it("looking for falsyness", function() {
+    expect('__').toBeFalsy();
   });
 
-  it("demonstrates equality",function() {
-    expect(1 + 1).toBe(2);
+  it("looking for equality",function() {
+    expect(1 + 1).toBe('__');
   });
 });
-/*
-test("ok", function() {
-    ok(__, 'what will satisfy the ok assertion?');
-});
-
-test("not", function() {
-    not(__, 'what is a false value?');
-});
-
-test("equals", function() {
-    equals(1+1, __, 'what will satisfy the equals assertion?');
-});
-*/
