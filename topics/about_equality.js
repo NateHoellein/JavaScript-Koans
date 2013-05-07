d4dd372dab2b50704eb02e48eb9660146df1f057
Nate Hoellein <natehoellein@gmail.com>
2013-05-07 11:33:04 -0400

    updated equality and truthyness

--- topics/about_equality.js
+++ topics/about_equality.js
@@ -1,45 +1,25 @@
 describe("About Equality", function() {
   it("shows numeric equality", function() {
-    expect(3 + 4).toBe(7);
+    expect(3 + __).toBe(7);
   });
 
   it("shows string equality", function() {
-    expect("3" + "7").toBe("37");
+    expect("3" + __).toBe("37");
   });
 
   it("shows equality without type coercion '==='", function() {
     var a = 3;
     var b = a;
-    expect(a).toBe(b); // 'what is exactly equal to 3?');
+    expect(a).toBe(__); // 'what is exactly equal to 3?');
   });
 
   it("shows equality with type coercion  '=='", function() {
     var a = 3;
     var b = a;
-    expect(a).toEqual(b);  // 'what string is equal to 3, with type coercion?');
+    expect(a).toEqual(__);  // 'what string is equal to 3, with type coercion?');
+  });
+
+  it("string literals", function() {
+      expect("frankenstein").toBe('__'); // "quote types are interchangable, but must match.");
   });
 });
-
-/*
-module("About Equality (topics/about_equality.js)");
-
-test("numeric equality", function() {
-    equals(3 + __,  7, 'hmmmm?');
-});
-
-test("string equality", function() {
-    equals("3" + __, "37", "concatenate the strings");
-});
-
-test("equality without type coercion", function() {
-    ok(3 === __, 'what is exactly equal to 3?');
-});
-
-test("equality with type coercion", function() {
-    ok(3 == "__", 'what string is equal to 3, with type coercion?');
-});
-
-test("string literals", function() {
-    equals("frankenstein", '__', "quote types are interchangable, but must match.");
-});
-*/
--- topics/about_truthyness.js
+++ topics/about_truthyness.js
@@ -2,44 +2,21 @@ describe("About Truthyness",function() {
   
   it("for positive numbers", function() {
     var oneIsTruthy = 1 ? true : false;
-    expect(oneIsTruthy).toBe(true);  // 'is one truthy?');
+    expect(oneIsTruthy).toBe(__);  // 'is one truthy?');
   });
 
   it("for negative numbers", function() {
     var negativeOneIsTruthy = -1 ? true : false;
-    expect(negativeOneIsTruthy).toBe(true);  // 'is -1 truthy?');
+    expect(negativeOneIsTruthy).toBe(__);  // 'is -1 truthy?');
   });
 
   it("for zero", function() {
     var zeroIsTruthy = 0 ? true : false;
-    expect(zeroIsTruthy).toBe(false);  // 'is 0 truthy?');
+    expect(zeroIsTruthy).toBe(__);  // 'is 0 truthy?');
   });
 
   it("for null", function() {
     var nullOneIsTruthy = null ? true : false;
-    expect(nullOneIsTruthy).toBe(false);  // 'is null truthy?');
+    expect(nullOneIsTruthy).toBe(__);  // 'is null truthy?');
   });
 });
-/*
-module("About Truthyness (topics/about_truthyness.js)");
-
-test("truthyness of positive numbers", function() {
-    var oneIsTruthy = 1 ? true : false;
-    equals(oneIsTruthy, __, 'is one truthy?');
-});
-
-test("truthyness of negative numbers", function() {
-    var negativeOneIsTruthy = -1 ? true : false;
-    equals(negativeOneIsTruthy, __, 'is -1 truthy?');
-});
-
-test("truthyness of zero", function() {
-    var zeroIsTruthy = 0 ? true : false;
-    equals(zeroIsTruthy, __, 'is 0 truthy?');
-});
-
-test("truthyness of null", function() {
-    var nullIsTruthy = null ? true : false;
-    equals(nullIsTruthy, __, 'is null truthy?');
-});
-*/
