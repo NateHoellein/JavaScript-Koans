9f12f1fb9894880505bb763be175e23d950636fa
Nate Hoellein <natehoellein@gmail.com>
2013-05-07 11:42:29 -0400

    updated topics/about_strings.js

--- topics/about_strings.js
+++ topics/about_strings.js
@@ -2,73 +2,37 @@ describe("About Strings (topics/about_strings.js)",function() {
   it("delimiters", function() {
       var singleQuotedString = 'apple';
       var doubleQuotedString = "apple";
-      expect(singleQuotedString).toBe(doubleQuotedString); // 'are the two strings equal?');
+      expect(singleQuotedString).toBe(__); // 'are the two strings equal?');
   });
   
   it("concatenation", function() {
       var fruit = "apple";
       var dish = "pie";
-      expect(fruit + " " + dish).toBe("apple pie"); // 'what is the value of fruit + " " + dish?');
+      expect(fruit + " " + dish).toBe(__); // 'what is the value of fruit + " " + dish?');
   });
   
   it("character Type", function() {
       var characterType = typeof("Amory".charAt(1)); // typeof will be explained in about reflection
-      expect(characterType).toBe("string"); // 'Javascript has no character type');
+      expect(characterType).toBe(__); // 'Javascript has no character type');
   });
   
   it("character Type", function() {
     var characterType = typeof("Amory".charAt(1)); // typeof will be explained in about reflection
-    expect(characterType).toBe("string"); // 'Javascript has no character type');
+    expect(characterType).toBe(__); // 'Javascript has no character type');
   });
 
   it("escape character", function() {
     var stringWithAnEscapedCharacter  = "\u0041pple";
-    expect(stringWithAnEscapedCharacter).toBe("Apple"); // 'what  is the value of stringWithAnEscapedCharacter?');
+    expect(stringWithAnEscapedCharacter).toBe(__); // 'what  is the value of stringWithAnEscapedCharacter?');
   });
 
   it("string.length", function() {
       var fruit = "apple";
-      expect(fruit.length).toBe(5); // 'what is the value of fruit.length?');
+      expect(fruit.length).toBe(__); // 'what is the value of fruit.length?');
   });
 
   it("slice", function() {
       var fruit = "apple pie";
-      expect(fruit.slice(0,5)).toBe("apple"); // 'what is the value of fruit.slice(0,5)?');
+      expect(fruit.slice(0,5)).toBe(__); // 'what is the value of fruit.slice(0,5)?');
   });
 });
-
-/*
-module("About Strings (topics/about_strings.js)");
-
-test("delimiters", function() {
-    var singleQuotedString = 'apple';
-    var doubleQuotedString = "apple";
-    equals(singleQuotedString === doubleQuotedString, __, 'are the two strings equal?');
-});
-
-test("concatenation", function() {
-    var fruit = "apple";
-    var dish = "pie";
-    equals(fruit + " " + dish, __, 'what is the value of fruit + " " + dish?');
-});
-
-test("character Type", function() {
-    var characterType = typeof("Amory".charAt(1)); // typeof will be explained in about reflection
-    equals(characterType, __, 'Javascript has no character type');
-});
-
-test("escape character", function() {
-    var stringWithAnEscapedCharacter  = "\u0041pple";
-    equals(stringWithAnEscapedCharacter, __, 'what  is the value of stringWithAnEscapedCharacter?');
-});
-
-test("string.length", function() {
-    var fruit = "apple";
-    equals(fruit.length, __, 'what is the value of fruit.length?');
-});
-
-test("slice", function() {
-    var fruit = "apple pie";
-    equals(fruit.slice(0,5), __, 'what is the value of fruit.slice(0,5)?');
-});
-*/
