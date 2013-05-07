25ace1bd16247be211e664d5c8ab4780732d328f
Nate Hoellein <natehoellein@gmail.com>
2013-05-07 14:13:37 -0400

    updated topics/about_arrays.js

--- topics/about_arrays.js
+++ topics/about_arrays.js
@@ -2,25 +2,25 @@ describe("About Arrays (topics/about_arrays.js)",function() {
   
   it("array literal syntax and indexing", function() {
       var favouriteThings = ["cellar door", 42, true]; // note that array elements do not have to be of the same type
-      expect(favouriteThings[0]).toBe("cellar door"); // 'what is in the first position of the array?');
-      expect(favouriteThings[1]).toBe(42); // 'what is in the second position of the array?');
-      expect(favouriteThings[2]).toBe(true); // 'what is in the third position of the array?');
+      expect(favouriteThings[0]).toBe(__); // 'what is in the first position of the array?');
+      expect(favouriteThings[1]).toBe(__); // 'what is in the second position of the array?');
+      expect(favouriteThings[2]).toBe(__); // 'what is in the third position of the array?');
   });
 
   it("array type", function() {
-      expect(typeof([])).toBe("object"); // 'what is the type of an array?');
+      expect(typeof([])).toBe(__); // 'what is the type of an array?');
   });
   
   it("length", function() {
     var collection = ['a','b','c'];
-    expect(collection.length).toBe(3); // 'what is the length of the collection array?');
+    expect(collection.length).toBe(__); // 'what is the length of the collection array?');
   });
 
   it("splice", function() {
       var daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-      var workingWeek = daysOfWeek.splice(0,5);
-      expect(workingWeek).toEqual(['Monday', 'Tuesday','Wednesday', 'Thursday','Friday']); // 'what is the value of workingWeek?');
-      expect(daysOfWeek).toEqual(['Saturday','Sunday']); //'what is the value of daysOfWeek?');
+      var workingWeek = daysOfWeek.splice(__,__);
+      expect(workingWeek).toEqual([__]); // 'what is the value of workingWeek?');
+      expect(daysOfWeek).toEqual([__]); //'what is the value of daysOfWeek?');
   });
 
   it("stack methods", function() {
@@ -28,8 +28,8 @@ describe("About Arrays (topics/about_arrays.js)",function() {
       stack.push("first");
       stack.push("second");
 
-      expect(stack.pop()).toBe("second"); // 'what will be the first value popped off the stack?');
-      expect(stack.pop()).toBe("first"); // 'what will be the second value popped off the stack?');
+      expect(stack.pop()).toBe(__); // 'what will be the first value popped off the stack?');
+      expect(stack.pop()).toBe(__); // 'what will be the second value popped off the stack?');
   });
 });
 
