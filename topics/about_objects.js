62ba0871cdd6b3e0bac7769e56f5288c5b2e71f2
Nate Hoellein <natehoellein@gmail.com>
2013-05-07 14:08:20 -0400

    updated topics/about_objects.js

--- topics/about_objects.js
+++ topics/about_objects.js
@@ -3,13 +3,13 @@ describe("About Objects (topics/about_objects.js)",function() {
 
   it("object type", function() {
       var empty_object = {};
-      expect(typeof(empty_object)).toBe("object"); // 'what is the type of an object?');
+      expect(typeof(empty_object)).toBe(__); // 'what is the type of an object?');
   });
 
   it("object literal notation", function() {
       var person = {
-          name:"Amory Blaine",
-          age:102
+          __:__,
+          __:__
       };		
       expect(person.name).toBe("Amory Blaine"); // 'what is the person\'s name?');
       expect(person.age).toBe(102); // 'what is the person\'s age?');
@@ -17,16 +17,16 @@ describe("About Objects (topics/about_objects.js)",function() {
 
   it("dynamically adding properties", function() {
       var person = {};
-      person.name = "Amory Blaine";
-      person.age = 102;
+      person.__ = "Amory Blaine";
+      person.__ = 102;
       expect(person.name).toBe("Amory Blaine"); // 'what is the person\'s name?');
       expect(person.age).toBe(102); // 'what is the person\'s age?');
   }); 
 
   it("adding properties from strings", function() {
       var person = {};
-      person["name"] = "Amory Blaine";
-      person["age"] = 102;
+      person["__"] = "Amory Blaine";
+      person["__"] = 102;
       expect(person.name).toBe("Amory Blaine"); // 'what is the person\'s name?');
       expect(person.age).toBe(102); // 'what is the person\'s age?');
   });
@@ -36,7 +36,7 @@ describe("About Objects (topics/about_objects.js)",function() {
           name: "Amory Blaine",
           age: 102,
           toString: function() {
-              return "I " + this.name + " am " + this.age + " years old.";  // HINT: use the 'this' keyword to refer to the person object.
+              return __;  // HINT: use the 'this' keyword to refer to the person object.
           }
       };
       expect(person.toString()).toBe("I Amory Blaine am 102 years old."); // 'what should the toString function be?');
