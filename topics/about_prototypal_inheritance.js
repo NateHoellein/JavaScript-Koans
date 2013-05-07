267a54fa95c057f136734fb82b314d78e7cb70f0
Nate Hoellein <natehoellein@gmail.com>
2013-05-07 14:27:43 -0400

    updated topics/about_prototypal_inheritance.js

--- topics/about_prototypal_inheritance.js
+++ topics/about_prototypal_inheritance.js
@@ -17,7 +17,7 @@ describe("About Prototypal Inheritance (topics/about_prototypal_inheritance.js)"
 
   it("defining a 'class'", function() {
       var eric  = new Mammal("Eric");
-      expect(eric.sayHi()).toBe("Hello, my name is Eric"); // 'what will Eric say?');
+      expect(eric.sayHi()).toBe(__); // 'what will Eric say?');
   });
 
 // add another function to the Mammal 'type' that uses the sayHi function
@@ -27,7 +27,7 @@ Mammal.prototype.favouriteSaying = function() {
 
   it("more functions", function() {
       var bobby = new Mammal("Bobby");
-      expect(bobby.favouriteSaying()).toBe("Bobby's favourite saying is Hello, my name is Bobby"); // "what is Bobby's favourite saying?"); 
+      expect(bobby.favouriteSaying()).toBe(__); // "what is Bobby's favourite saying?"); 
   });
 
   it("calling functions added to a prototype after an object was created", function() {
@@ -37,7 +37,7 @@ Mammal.prototype.favouriteSaying = function() {
       };
       // the following statement asks the paul object to call a function that was added
       // to the Mammal prototype after paul was constructed.
-      expect(paul.numberOfLettersInName()).toBe(4); // "how long is Paul's name?");
+      expect(paul.numberOfLettersInName()).toBe(__); // "how long is Paul's name?");
   });
 
 // helper function for inheritance. 
@@ -57,67 +57,7 @@ extend(Bat, Mammal);
 
   it("Inheritance", function() {
       var lenny = new Bat("Lenny", "1.5m");
-      expect(lenny.sayHi()).toBe("Hello, my name is Lenny"); // "what does Lenny say?");
-      expect(lenny.wingspan).toBe("1.5m"); // "what is Lenny's wingspan?");
+      expect(lenny.sayHi()).toBe(__); // "what does Lenny say?");
+      expect(lenny.wingspan).toBe(__); // "what is Lenny's wingspan?");
   });
 });
-/*
-module("About Prototypal Inheritance (topics/about_prototypal_inheritance.js)");
-
-// this 'class' pattern defines a class by its constructor
-var Mammal = function(name) {
-    this.name = name;
-}
-// things that don't need to be set in the constructor should be added to the constructor's prototype property.
-Mammal.prototype = {
-    sayHi: function() {
-        return "Hello, my name is " + this.name;
-    }
-}
-
-test("defining a 'class'", function() {
-    var eric  = new Mammal("Eric");
-    equals(eric.sayHi(), __, 'what will Eric say?');
-});
-
-// add another function to the Mammal 'type' that uses the sayHi function
-Mammal.prototype.favouriteSaying = function() {
-    return this.name + "'s favourite saying is " + this.sayHi(); 
-}
-
-test("more functions", function() {
-    var bobby = new Mammal("Bobby");
-    equals(bobby.favouriteSaying(), __, "what is Bobby's favourite saying?"); 
-});
-
-test("calling functions added to a prototype after an object was created", function() {
-    var paul = new Mammal("Paul");
-    Mammal.prototype.numberOfLettersInName = function() {
-        return this.name.length;
-    };
-    // the following statement asks the paul object to call a function that was added
-    // to the Mammal prototype after paul was constructed.
-    equals(paul.numberOfLettersInName(), __, "how long is Paul's name?");
-});
-
-// helper function for inheritance. 
-// From https://developer.mozilla.org/en/JavaScript/Guide/Inheritance_Revisited
-function extend(child, supertype){  
-    child.prototype = supertype.prototype;  
-} 
-
-// "Subclass" Mammal
-function Bat(name, wingspan) {
-    Mammal.call(this, name);
-    this.wingspan = wingspan;
-}	
-
-// configure inheritance
-extend(Bat, Mammal);
-
-test("Inheritance", function() {
-    var lenny = new Bat("Lenny", "1.5m");
-    equals(lenny.sayHi(), __, "what does Lenny say?");
-    equals(lenny.wingspan, __, "what is Lenny's wingspan?");
-});
-*/
