5d678ffc5687ffbdf0ace236093401d98990e6e7
Nate Hoellein <natehoellein@gmail.com>
2013-05-07 14:46:49 -0400

    updated topics/about_regular_expressions.jsc

--- topics/about_regular_expressions.js
+++ topics/about_regular_expressions.js
@@ -4,61 +4,29 @@ describe("About Regular Expressions (topics/about_regular_expressions.js)",funct
   it("exec", function() {
       var numberFinder = /(\d).*(\d)/;
       var results = numberFinder.exec("what if 6 turned out to be 9?");
-      expect(results).toMatch(['6 turned out to be 9','6','9']); // 'what is the value of results?');		
+      expect(results).toMatch([__,__,__]); // 'what is the value of results?');		
   });
 
   it("test", function() {
       var containsSelect = /select/.test("  select * from users ");
-      expect(containsSelect).toBeTruthy(); // 'does the string provided contain "select"?');
+      expect(containsSelect).toBe(__); // 'does the string provided contain "select"?');
   });
 
   it("match", function() {
       var matches = "what if 6 turned out to be 9?".match(/(\d)/g);
-      expect(matches).toMatch(["6",'9']); // 'what is the value of matches?');
+      expect(matches).toMatch([__,__]); // 'what is the value of matches?');
   });
 
   it("replace", function() {
       var pie = "apple pie".replace("apple", "strawberry");
-      expect(pie).toBe('strawberry pie'); // 'what is the value of pie?');
+      expect(pie).toBe(__); // 'what is the value of pie?');
 
       pie = "what if 6 turned out to be 9?".replace(/\d/g, function(number) { // the second parameter can be a string or a function
           var map = {'6': 'six','9': 'nine'};
           return map[number];
       });
-      expect(pie).toBe('what if six turned out to be nine?'); // 'what is the value of pie?');
+      expect(pie).toBe(__); // 'what is the value of pie?');
   });
 // THE END
 
 });
-
-/*
-module("About Regular Expressions (topics/about_regular_expressions.js)");
-
-test("exec", function() {
-    var numberFinder = /(\d).*(\d)/;
-    var results = numberFinder.exec("what if 6 turned out to be 9?");
-    ok(results.equalTo([__, __, __]), 'what is the value of results?');		
-});
-
-test("test", function() {
-    var containsSelect = /select/.test("  select * from users ");
-    equals(containsSelect, __, 'does the string provided contain "select"?');
-});
-
-test("match", function() {
-    var matches = "what if 6 turned out to be 9?".match(/(\d)/g);
-    ok(matches.equalTo([__, __]), 'what is the value of matches?');
-});
-
-test("replace", function() {
-    var pie = "apple pie".replace("apple", "strawberry");
-    equals(pie, __, 'what is the value of pie?');
-
-    pie = "what if 6 turned out to be 9?".replace(/\d/g, function(number) { // the second parameter can be a string or a function
-        var map = {'6': 'six','9': 'nine'};
-        return map[number];
-    });
-    equals(pie, __, 'what is the value of pie?');
-});
-*/
-// THE END
--- topics/about_scope.js
+++ topics/about_scope.js
@@ -4,7 +4,7 @@ describe("About Scope (topics/about_scope.js)",function() {
   thisIsAGlobalVariable = 77;
 
   it("global variables", function() {
-      expect(thisIsAGlobalVariable).toBe(77); // 'is thisIsAGlobalVariable defined in this scope?');
+      expect(thisIsAGlobalVariable).toBe(__); // 'is thisIsAGlobalVariable defined in this scope?');
   });
 
   it("variables declared inside of a function", function() {
@@ -13,34 +13,11 @@ describe("About Scope (topics/about_scope.js)",function() {
       // this is a self-invoking function. Notice that it calls itself at the end ().
       (function() {
           var innerVariable = "inner";
-          expect(outerVariable).toBe("outer"); // 'is outerVariable defined in this scope?');
-          expect(innerVariable).toBe("inner"); // 'is innerVariable defined in this scope?');
+          expect(outerVariable).toBe(__); // 'is outerVariable defined in this scope?');
+          expect(innerVariable).toBe(__); // 'is innerVariable defined in this scope?');
       })();
 
-      expect(outerVariable).toBe("outer"); // 'is outerVariable defined in this scope?');
-      expect(typeof(innerVariable)).toBe('undefined'); // 'is innerVariable defined in this scope?');
+      expect(outerVariable).toBe(__); // 'is outerVariable defined in this scope?');
+      expect(typeof(innerVariable)).toBe(__); // 'is innerVariable defined in this scope?');
   });
 });
-/*
-module("About Scope (topics/about_scope.js)");
-
-thisIsAGlobalVariable = 77;
-
-test("global variables", function() {
-    equals(thisIsAGlobalVariable, __, 'is thisIsAGlobalVariable defined in this scope?');
-});
-
-test("variables declared inside of a function", function() {
-    var outerVariable = "outer";
-
-    // this is a self-invoking function. Notice that it calls itself at the end ().
-    (function() {
-        var innerVariable = "inner";
-        equals(outerVariable, __, 'is outerVariable defined in this scope?');
-        equals(innerVariable, __, 'is innerVariable defined in this scope?');
-    })();
-
-    equals(outerVariable, __, 'is outerVariable defined in this scope?');
-    equals(typeof(innerVariable), __, 'is innerVariable defined in this scope?');
-});
-*/
