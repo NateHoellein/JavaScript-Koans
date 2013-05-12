efd2e65c022bf5336104b1dcd89bd1c715279183
Nate Hoellein <natehoellein@gmail.com>
2013-05-12 08:33:54 -0400

    refactored handling of 'incomplete'

--- support/koans.js
+++ support/koans.js
@@ -6,38 +6,25 @@ var originalToBe = jasmine.Matchers.prototype.toBe;
 var originalToEqual = jasmine.Matchers.prototype.toEqual;
 var originalToMatch = jasmine.Matchers.prototype.toMatch;
 
-jasmine.Matchers.prototype.toBe = function(expected) {
-  var that = this;
-  if(expected === __ || this.actual.indexOf(__)) {
-      this.message = function() {
+var OverrideExpect = function(that, expected, originalExpect) {
+  if(expected === __ || that.actual.indexOf(__)) {
+      that.message = function() {
         return "I see this spec is incomplete, you have more work to do.";
       }
       return false;
     } else {
-      return originalToBe.call(that,expected);  
+      return originalExpect.call(that,expected);  
     };
+};
+
+jasmine.Matchers.prototype.toBe = function(expected) {
+  OverrideExpect(this, expected,originalToBe);
 };
 
 jasmine.Matchers.prototype.toEqual = function(expected) {
-  var that = this;
-  if(expected === __) {
-      this.message = function() {
-        return "I see this spec is incomplete, you have more work to do.";
-      }
-      return false;
-    } else {
-      return originalToEqual.call(that,expected);  
-    };
+  OverrideExpect(this,expected,originalToEqual);
 };
 
 jasmine.Matchers.prototype.toMatch = function(expected) {
-  var that = this;
-  if(expected === __) {
-      this.message = function() {
-        return "I see this spec is incomplete, you have more work to do.";
-      }
-      return false;
-    } else {
-      return originalToMatch.call(that,expected);  
-    };
+  OverrideExpect(this,expected, originalToMatch);
 };
