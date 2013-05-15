1768ec606a157c02799426ea3f4c95790c922133
Nate Hoellein <natehoellein@gmail.com>
2013-05-15 08:55:33 -0400

    adding tests for koans overrides looking for __

--- support/koans.js
+++ support/koans.js
@@ -7,24 +7,39 @@ var originalToEqual = jasmine.Matchers.prototype.toEqual;
 var originalToMatch = jasmine.Matchers.prototype.toMatch;
 
 var OverrideExpect = function(that, expected, originalExpect) {
-  if(expected === __ || that.actual.indexOf(__)) {
+  if(literalTest(expected,that.actual)) {
       that.message = function() {
-        return "I see this spec is incomplete, you have more work to do.";
+        return "Oops this spec has not been fixed, you have more work to do.";
       }
       return false;
-    } else {
+    } 
+    if(stringTest(expected,that.actual)) {
+      that.message = function() {
+        return "Oops this spec has not been fixed, you have more work to do.";
+      };
+      return false;
+    }  
+    else {
       return originalExpect.call(that,expected);  
     };
 };
 
 jasmine.Matchers.prototype.toBe = function(expected) {
-  OverrideExpect(this, expected,originalToBe);
+  return OverrideExpect(this, expected,originalToBe);
 };
 
 jasmine.Matchers.prototype.toEqual = function(expected) {
-  OverrideExpect(this,expected,originalToEqual);
+  return OverrideExpect(this,expected,originalToEqual);
 };
 
 jasmine.Matchers.prototype.toMatch = function(expected) {
-  OverrideExpect(this,expected, originalToMatch);
+  return OverrideExpect(this,expected, originalToMatch);
+};
+
+var literalTest = function(expected,actual) {
+  return expected === __ || (actual.indexOf(__) >= 0); 
+};
+
+var stringTest = function(expected, actual) {
+  return expected === '__' || (actual.indexOf('__') >= 0); 
 };
