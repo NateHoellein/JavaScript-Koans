a568c0b0cca9b5c4b6d4bd5ae469c6bb9cdede68
Nate Hoellein <natehoellein@gmail.com>
2013-05-15 13:22:08 -0400

    removed some tests that didn't apply anymore

--- support/koans_spec.js
+++ support/koans_spec.js
@@ -1,106 +1,92 @@
 describe("Spec for koans support",function() {
-  xit("checks for '__' as expected", function() {
+  it("checks for '__' as expected", function() {
     this.actual = "something that doesn't match";
     var result = OverrideExpect(this,__,function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for '__' as actual", function() {
+  it("checks for '__' as actual", function() {
     this.actual = __;
     var result = OverrideExpect(this,__,function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for '__' within 'actual' string ", function() {
+  it("checks for '__' within 'actual' string ", function() {
     this.actual = "something that doesn't __ match";
     var result = OverrideExpect(this,"boo",function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for '__' as the expected result", function() {
+  it("checks for '__' as the expected result", function() {
     this.actual = "something that doesn't match";
     var result = OverrideExpect(this,'__',function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for '__' within the expected result", function() {
+  it("checks for '__' within the expected result", function() {
     this.actual = "something that doesn't match";
     var result = OverrideExpect(this,'hey, I have a __ in me',function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for '__' as the actual result", function() {
+  it("checks for '__' as the actual result", function() {
     this.actual = "__";
     var result = OverrideExpect(this,'expected',function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for '__' as expected when dealing with numbers", function() {
+  it("checks for '__' as expected when dealing with numbers", function() {
     this.actual = 15;
     var result = OverrideExpect(this,__,function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for '__' as actual when dealing with numbers", function() {
+  it("checks for '__' as actual when dealing with numbers", function() {
     this.actual = __;
     var result = OverrideExpect(this,15,function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for '__' in the actual object value", function() {
+  it("checks for '__' in the actual object value", function() {
     this.actual = {foo: "__"};
     var expected = {foo: "bar"};
     var result = OverrideExpect(this,expected,function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for a literal '__' in the actual object value", function() {
+  it("checks for a literal '__' in the actual object value", function() {
     this.actual = {foo: __};
     var expected = {foo: "bar"};
     var result = OverrideExpect(this,expected,function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for '__' in the expected object value", function() {
+  it("checks for '__' in the expected object value", function() {
     this.actual = {foo: "bar"};
     var expected = {foo: "__"};
     var result = OverrideExpect(this,expected,function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for a literal '__' in the expected object value", function() {
+  it("checks for a literal '__' in the expected object value", function() {
     this.actual = {foo: "bar"};
     var expected = {foo: __};
     var result = OverrideExpect(this,expected,function() {});
     verifyResults(this.message.call(),result);
   });
 
-  xit("checks for a literal '__' in the expected object property", function() {
+  it("checks for a literal '__' in the expected object property", function() {
     this.actual = {foo: "bar"};
     var expected = {__: "myvalue"};
     var result = OverrideExpect(this,expected,function() {});
     verifyResults(this.message.call(),result);
   });
   
-  xit("checks for a '__' in the expected object property", function() {
+  it("checks for a '__' in the expected object property", function() {
     this.actual = {foo: "bar"};
     var expected = {"__": "myvalue"};
     var result = OverrideExpect(this,expected,function() {});
     verifyResults(this.message.call(),result);
-  });
-
-  it("object literal notation", function() {
-      var person = {
-          __:__,
-          __:__
-      };		
-      this.actual = person;
-      expect(person.name).toBe("Amory Blaine"); // 'what is the person\'s name?');
-      expect(person.age).toBe(102); // 'what is the person\'s age?');
-  });
-
-  xit("checks for __ during equality tests",function() {
-    expect(3 + __).toBe(7);
   });
 
   var verifyResults = function(failureMessage, result) {
